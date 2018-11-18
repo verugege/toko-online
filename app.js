b4641369cdd6a66e09352d672a198c4220fac73f
verrru <verugege@gmail.com>
2018-11-18 14:19:57 +0700

    tambah seesion dan body parser

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var path = require('path');
 var mongoose = require('mongoose');
 var config = require("./config/database");
+var bodyParser = require("body-parser");
+var session = require("express-session");
 
 //connection syntax
 mongoose.connect(config.database);
@@ -32,6 +34,24 @@ var adminPages = require("./routes/admin_pages.js");
 // redirect 
 app.use('/', pages);
 app.use("/admin/pages", adminPages);
+
+/** MIDDLEWARE */
+// Setup body parser midleware
+app.use(bodyParser.urlencoded({extended:
+  false
+}));
+app.use(bodyParser.json());
+
+//setup session middleware 
+//fungsinya pengamnan session user
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true}
+  })
+);
 
 //setup server
 var port = 3000;
