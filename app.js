28ec5586485191a7529daee09eab20f2df6a9ad7
verrru <verugege@gmail.com>
2018-11-18 10:45:00 +0700

    membuat folder config database, memindahkan konfigurasi sintax kedalam folder config

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 var express = require("express");
 var path = require('path');
+var mongoose = require('mongoose');
+var config = require("./config/database");
+
+//connection syntax
+mongoose.connect(config.database);
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console,"connection error"));
+
+db.once("open",function(){
+console.log("Sudah Connect Ke Mongodb Coy");
+});
 
 //initial app
 var app = express();
 
+
 //view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
-// Setup public folder untuk independent file seperti template dll
+//setup public folder untuk independent file seperti template dll
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Setup Index dengan metode get
-app.get('/', function(req, res){
-  res.send("Ini adalah index");
-});
 
-/// Setup server
+//setup index dengan metode get
+app.get('/', function(req, res){
+    res.send("Ini adalah index yoyo dan sudah connect ke mongodb");
+    });
+
+//setup server
 var port = 3000;
 app.listen(port,function(){
-  console.log("Server Running on Port " + port);
+console.log("Server Runnning Port " + port);
 });
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports={
+  datanase: "mongodb://localhost/toko-online"
+};
